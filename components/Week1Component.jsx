48c5489cd7facf92e840e708a77aca8664ad5aec
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2024-01-14 20:45:15 -0700

    Class Demo setup
    Class Demo Setup

---
+++ components/Week1Component.jsx
@@ -0,0 +1,12 @@
+// Week1.js
+const Week1Component = () => {
+  return (
+    <div>
+      <div className="flex bg-blue-100 w-full h-screen">
+        This is a test. This is a very good test of Week 1.
+      </div>
+    </div>
+  );
+};
+
+export default Week1Component;
---
+++ components/Week2Component.jsx
@@ -0,0 +1,12 @@
+// Week2.js
+const Week2Component = () => {
+  return (
+    <div>
+      <div className="flex bg-blue-100 w-full h-screen">
+        This is a test. This is a very good test
+      </div>
+    </div>
+  );
+};
+
+export default Week2Component;
