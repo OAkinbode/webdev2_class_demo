bff442bd86bc80cab53188fbf6618cc9ae6c61a3
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2024-03-10 19:13:27 -0600

    changes made

--- next.config.js
+++ next.config.js
@@ -10,8 +10,7 @@
 //   },
 // };
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   async headers() {
     return [
       {
@@ -19,13 +18,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3000",
-          }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
@@ -37,5 +33,3 @@ const nextConfig = {
     ];
   },
 };
-
-module.exports = nextConfig;
