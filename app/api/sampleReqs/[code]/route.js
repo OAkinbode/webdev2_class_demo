88ac46f07f32022d92c34605cddff7d0237d17d6
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2024-03-12 16:03:49 -0600

    changes made

--- app/api/sampleReqs/[code]/route.js
+++ app/api/sampleReqs/[code]/route.js
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params: { code } }) {
-  const secretCode = process.env.SECRET_CLASS_PASSCODE;
+  const secretCode =
+    code === "ehen2rfow"
+      ? process.env.SECRET_CLASS_PASSCODE
+      : "You passcode is not correct";
+
   return NextResponse.json({ secretCode });
 }
