747bf9624f4008e03020b02e27e0b04e814766ad
Sola Akinbode <104801652+OAkinbode@users.noreply.github.com>
2024-03-21 14:54:38 -0600

    changes made

--- app/page.js
+++ app/page.js
@@ -32,33 +32,33 @@ export default function Home() {
     return number1 + number2;
   };
 
-  if (!user) {
-    return (
-      <main className="min-h-screen ">
-        <div className="items-center justify-center p-2 gap-2 flex flex-row">
-          <div>You are logged out: Click this button to log back in</div>
-          <button className="bg-green-600 text-white p-2" onClick={signIn}>
-            {" "}
-            login{" "}
-          </button>
+  // if (!user) {
+  //   return (
+  //     <main className="min-h-screen ">
+  //       <div className="items-center justify-center p-2 gap-2 flex flex-row">
+  //         <div>You are logged out: Click this button to log back in</div>
+  //         <button className="bg-green-600 text-white p-2" onClick={signIn}>
+  //           {" "}
+  //           login{" "}
+  //         </button>
+  //       </div>
+  //     </main>
+  //   );
+  // } else {
+  return (
+    <main className="min-h-screen ">
+      <Header />
+      <div className="items-center justify-center p-2 gap-2 flex flex-row">
+        <SidePane />
+        <div className="text-xl text-gray-600 flex-1 bg-white h-screen border-2 border-yellow-700">
+          <Dashboard />
         </div>
-      </main>
-    );
-  } else {
-    return (
-      <main className="min-h-screen ">
-        <Header />
-        <div className="items-center justify-center p-2 gap-2 flex flex-row">
-          <SidePane />
-          <div className="text-xl text-gray-600 flex-1 bg-white h-screen border-2 border-yellow-700">
-            <Dashboard />
-          </div>
-          <div className="text-xl text-gray-600 flex-1 bg-white h-screen border-2 border-yellow-700">
-            <DatabaseCrud />
-          </div>
+        <div className="text-xl text-gray-600 flex-1 bg-white h-screen border-2 border-yellow-700">
+          <DatabaseCrud />
         </div>
-        <Footer />
-      </main>
-    );
-  }
+      </div>
+      <Footer />
+    </main>
+  );
+  // }
 }
